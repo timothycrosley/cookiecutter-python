74f0b4c8da4ce123f53c859d803f451f00fcadd3
Timothy Crosley <timothy.crosley@gmail.com>
2019-10-13 23:35:56 -0700

    Set flags after bash header

--- {{cookiecutter.project_name}}/scripts/clean.sh
+++ {{cookiecutter.project_name}}/scripts/clean.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -xe
+#!/bin/bash
+set -euxo pipefail
 
 poetry run isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --use-parentheses --line-width=100 --recursive {{cookiecutter.project_name}}/ tests/
 poetry run black {{cookiecutter.project_name}}/ tests/ -l 100
--- {{cookiecutter.project_name}}/scripts/test.sh
+++ {{cookiecutter.project_name}}/scripts/test.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -xe
+#!/bin/bash
+set -euxo pipefail
 
 ./scripts/lint.sh
 poetry run pytest -s --cov={{cookiecutter.project_name}}/ --cov=tests --cov-report=term-missing ${@} --cov-report html
