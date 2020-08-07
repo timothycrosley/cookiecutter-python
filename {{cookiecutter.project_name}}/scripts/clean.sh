ea8f733f85e7089df338d41ace199d3f4d397e29
Timothy Edmund Crosley <timothy.crosley@gmail.com>
2020-08-06 23:55:24 -0700

    Update clean.sh

--- {{cookiecutter.project_name}}/scripts/clean.sh
+++ {{cookiecutter.project_name}}/scripts/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 
-poetry run isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --use-parentheses --line-width=100 --recursive {{cookiecutter.project_name}}/ tests/
+poetry run isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --use-parentheses --line-width=100 {{cookiecutter.project_name}}/ tests/
 poetry run black {{cookiecutter.project_name}}/ tests/ -l 100
