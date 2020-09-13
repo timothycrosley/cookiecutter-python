ff6836bfaa247c65ff50b39c520ed12d91bf5a20
Timothy Crosley <timothy.crosley@gmail.com>
2020-09-12 23:52:56 -0700

    Fix setting that cant be applied via simple config

--- {{cookiecutter.project_name}}/scripts/lint.sh
+++ {{cookiecutter.project_name}}/scripts/lint.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 poetry run cruft check
-poetry run mypy {{cookiecutter.project_name}}/
+poetry run mypy --ignore-missing-imports {{cookiecutter.project_name}}/
 poetry run isort --check --diff {{cookiecutter.project_name}}/ tests/
 poetry run black --check {{cookiecutter.project_name}}/ tests/
 poetry run flake8 {{cookiecutter.project_name}}/ tests/
