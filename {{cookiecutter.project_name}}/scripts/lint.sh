63b610f948ef70a25dfa288a0a914c510ee6005f
Timothy Crosley <timothy.crosley@gmail.com>
2020-09-12 23:50:12 -0700

    Move all configs into config files to improve usage within editors

--- {{cookiecutter.project_name}}/scripts/lint.sh
+++ {{cookiecutter.project_name}}/scripts/lint.sh
@@ -2,9 +2,9 @@
 set -euxo pipefail
 
 poetry run cruft check
-poetry run mypy --ignore-missing-imports {{cookiecutter.project_name}}/
-poetry run isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --use-parentheses --line-width=100 --check --diff --recursive {{cookiecutter.project_name}}/ tests/
-poetry run black --check -l 100 {{cookiecutter.project_name}}/ tests/
-poetry run flake8 {{cookiecutter.project_name}}/ tests/ --max-line 100 --ignore F403,F401,W503,E203
+poetry run mypy {{cookiecutter.project_name}}/
+poetry run isort --check --diff {{cookiecutter.project_name}}/ tests/
+poetry run black --check {{cookiecutter.project_name}}/ tests/
+poetry run flake8 {{cookiecutter.project_name}}/ tests/
 poetry run safety check
 poetry run bandit -r {{cookiecutter.project_name}}/
