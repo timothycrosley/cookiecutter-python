9be01014cde7ec06bd52415655d52ca30a9e9bcc
Timothy Crosley <timothy.crosley@gmail.com>
2021-02-03 22:31:45 -0800

    Ignore tornado red herring vulnerability

--- {{cookiecutter.project_name}}/scripts/lint.sh
+++ {{cookiecutter.project_name}}/scripts/lint.sh
@@ -6,5 +6,5 @@ poetry run mypy --ignore-missing-imports {{cookiecutter.project_name}}/
 poetry run isort --check --diff {{cookiecutter.project_name}}/ tests/
 poetry run black --check {{cookiecutter.project_name}}/ tests/
 poetry run flake8 {{cookiecutter.project_name}}/ tests/
-poetry run safety check
+poetry run safety check -i 39462
 poetry run bandit -r {{cookiecutter.project_name}}/
